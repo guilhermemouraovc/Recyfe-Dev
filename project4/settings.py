c5e5b608066e100a25053e3aa56562fa0053e114
Henrique Figueiredo <hft@cesar.school>
2024-10-28 21:51:42 -0300

    minor corrections

--- project4/settings.py
+++ project4/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
